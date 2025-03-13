6d2ef4900b42db189f06de8c17daa6de9342b54c
pattananNP <phattanun19@hotmail.com>
2025-03-13 17:52:27 +0700

    add crud

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -1,28 +1,47 @@
-<nav class="bg-white sticky top-0 z-50 h-full">
-    <div class="container mx-auto flex justify-between items-center flex-col">
-        <!-- Searchbox -->
-        <div class="flex flex-col space-y-2">
-            <div class="flex items-center justify-between w-full p-2">
-                <h1 class="text-2xl font-extrabold text-start">
-                    <span class="text-rose-500">MU</span>|Directory
+<nav class="bg-white sticky top-0 z-50 shadow-sm">
+    <div class="container mx-auto py-2">
+        <div class="flex items-center justify-between">
+            <!-- Logo -->
+            <div class="flex items-center">
+                <h1 class="text-2xl font-extrabold">
+                    <a
+                        href="{{ route('home') }}"
+                        class="flex items-center"
+                    >
+                        <span class="text-rose-500">MU</span><span
+                            class="text-gray-800">|Directory</span>
+                    </a>
                 </h1>
-                @include('components.searchbox')
             </div>
-
-
-        </div>
-
-        {{-- @auth
-
-        @else
-        <a
-            href="{{ route('login') }}"
-            class="px-4 py-2 border rounded-md"
-        >Login</a>
-        <a
-            href="{{ route('register') }}"
-            class="px-4 py-2 bg-blue-500 text-white rounded-md"
-        >Sign Up</a>
-        @endauth --}}
+            
+            <!-- Search Box (only on home and search pages) -->
+            @if(Route::is('home') || Route::is('sacredplaces.index'))
+                <div class="flex-1 max-w-xl mx-4">
+                    @include('components.search-box')
+                </div>
+            @endif
+            
+            <!-- Navigation Links -->
+            <div class="flex items-center space-x-4">
+                <a
+                    href="{{ route('sacredplaces.create') }}"
+                    class="text-gray-700 hover:text-blue-600 font-medium"
+                >
+                    Add Place
+                </a>
+                {{-- @auth
+                    <!-- User dropdown would go here -->
+                    @else
+                    <a
+                        href="{{ route('login') }}"
+                        class="px-4 py-2 border rounded-md"
+                    >Login</a>
+                    <a
+                        href="{{ route('register') }}"
+                        class="px-4 py-2 bg-blue-500 text-white rounded-md"
+                    >Sign Up</a>
+                    @endauth --}}
+            </div>
+            </div>
     </div>
 </nav>
